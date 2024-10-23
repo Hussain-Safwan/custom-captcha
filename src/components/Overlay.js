7e6533bd17b6d64176c528593fbb3351adfe18d1
Hussain Safwan <safwan.du16@gmail.com>
2024-10-23 13:46:37 +0600

    overlay: icon rendering

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -3,48 +3,20 @@ import "../styles/overlay.css";
 import ChangeHistoryIcon from "@mui/icons-material/ChangeHistory";
 import SquareIcon from "@mui/icons-material/CropSquareOutlined";
 import CircleIcon from "@mui/icons-material/CircleOutlined";
-const Icon = ({ setPosition, position }) => {
-  const empty = Math.floor(Math.random() * 10) % 2;
-  if (empty === 0) {
-    setPosition(position, { empty: true });
-    return <></>;
-  }
 
-  const style = { padding: "10px 0" };
-  const iconType = Math.floor(Math.random() * 10) % 3;
-  const tintType = Math.floor(Math.random() * 10) % 3;
-  const iconTint = ["error", "success", "secondary"];
-  const iconList = [
-    <ChangeHistoryIcon style={style} color={iconTint[tintType]} />,
-    <SquareIcon style={style} color={iconTint[tintType]} />,
-    <CircleIcon style={style} color={iconTint[tintType]} />,
-  ];
-
-  const colorTranslate = (type) => {
-    if (type === 0) return "red";
-    else if (type === 1) return "green";
-    return "blue";
+const Icon = ({ icon }) => {
+  console.log(icon);
+  const colorMap = {
+    red: "error",
+    green: "success",
+    blue: "secondary",
   };
 
-  if (iconType === 0)
-    setPosition(position, {
-      empty: false,
-      iconShape: "triangle",
-      iconTint: colorTranslate(tintType),
-    });
-  else if (iconType === 1)
-    setPosition(position, {
-      empty: false,
-      iconShape: "square",
-      iconTint: colorTranslate(tintType),
-    });
-  else
-    setPosition(position, {
-      empty: false,
-      iconShape: "circle",
-      iconTint: colorTranslate(tintType),
-    });
-  return iconList[iconType];
+  if (icon.iconShape === "triangle")
+    return <ChangeHistoryIcon color={colorMap[icon.iconTint]} />;
+  else if (icon.iconShape === "square")
+    return <SquareIcon color={colorMap[icon.iconTint]} />;
+  return <CircleIcon color={colorMap[icon.iconTint]} />;
 };
 
 const Overlay = ({ table, location, setIconPosition, handleSectionClick }) => {
@@ -56,13 +28,7 @@ const Overlay = ({ table, location, setIconPosition, handleSectionClick }) => {
       {table.map((item, i) => (
         <div className="section" onClick={() => handleSectionClick(item)}>
           {i + 1}
-          {!item.empty && (
-            <Icon
-              setPosition={setIconPosition}
-              position={item}
-              style={{ paddingTop: "20px" }}
-            />
-          )}
+          {!item.empty && <Icon icon={item} />}
         </div>
       ))}
     </div>
