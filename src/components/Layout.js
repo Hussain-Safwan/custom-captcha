b446c56fa4dfd1f0f440196a9896592018ecef98
Hussain Safwan <safwan.du16@gmail.com>
2024-10-23 13:37:23 +0600

    table creation moved up

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,31 @@ import VideoFeed from "./VideoFeed";
 import "../styles/layout.css";
 
 const Layout = () => {
+  const randomInt = (rem) => {
+    return Math.floor(Math.random() * 10) % rem;
+  };
+  const iconShapes = ["trianle", "square", "circle"];
+  const iconTints = ["red", "green", "blue"];
+
+  let table = Array(25).fill(0);
+  table.forEach((item, i) => {
+    const empty = randomInt(2);
+    if (empty) {
+      table[i] = { empty: true };
+    } else {
+      const type = randomInt(3);
+      table[i] = {
+        empty: false,
+        iconShape: iconShapes[type],
+        iconTint: iconTints[type],
+      };
+    }
+  });
+
   return (
     <div className="wrapper">
       <div className="feed">
-        <VideoFeed />
+        <VideoFeed table={table} />
       </div>
     </div>
   );
